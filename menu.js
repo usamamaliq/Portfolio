a50f8fbb5767e492266f835838d094a02863d403
Usama Masood <usamamaliq@gmail.com>
2022-09-13 01:31:14 +0500

    Add javascript for nav menu to close menu with x button and when nav items clicked

--- menu.js
+++ menu.js
@@ -1,9 +1,16 @@
 const menuBtn = document.querySelector('#menu');
-const closeBtn = document.querySelector('.close-btn');
+const closeBtn = document.querySelector('.closeBtn');
 const nav = document.querySelector('.desktop-menu');
 
 menuBtn.addEventListener('click', () => {
   nav.classList.add('menu-show');
 });
 
+closeBtn.addEventListener('click', () => {
+  nav.classList.remove('menu-show');
+});
 
+document.querySelectorAll('.menu-item').forEach((n) => (
+  n.addEventListener('click', () => {
+    nav.classList.remove('menu-show');
+  })));

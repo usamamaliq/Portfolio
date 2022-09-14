6d7d448b120af281e62f5b098e1771a9a790f5b9
Usama Masood <usamamaliq@gmail.com>
2022-09-15 03:32:29 +0500

    Add style for popup screen

--- popup.js
+++ popup.js
@@ -1,100 +1,99 @@
-const worksInfo = [ 
- {
-    projectId : 1,
-    gridClass : "w1",
+const worksInfo = [
+  {
+    projectId: 1,
+    gridClass: 'w1',
     name: 'Keeping tracks of hunders of components',
     projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     imageDesktop: './Images/Snapshoot-Portfolio.svg',
     imageMobile: './Images/mobile-snapshoot.svg',
-    githubIcon:'./Images/gitHubIcon.png',
+    githubIcon: './Images/gitHubIcon.png',
     liveIcon: './Images/liveIcon.svg',
-    technologiesMobile: ['Ruby on Rails','CSS','JavaScript'],
-    technologiesDesktop: ['Codekit','Github','JavaScript','Bootstrap','Terminal','Codepen'],
+    technologiesMobile: ['Ruby on Rails', 'CSS', 'JavaScript'],
+    technologiesDesktop: ['Codekit', 'Github', 'JavaScript', 'Bootstrap', 'Terminal', 'Codepen'],
     sourceLink: '#',
     liveLink: '#',
-},
+  },
 
-{
-    projectId : 2,
-    gridClass : "w2",
+  {
+    projectId: 2,
+    gridClass: 'w2',
     name: 'Keeping tracks of hunders of components',
     projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     imageDesktop: './Images/Snapshoot-Portfolio.svg',
     imageMobile: './Images/mobile-snapshoot.svg',
-    githubIcon:'./Images/gitHubIcon.png',
+    githubIcon: './Images/gitHubIcon.png',
     liveIcon: './Images/liveIcon.svg',
-    technologiesMobile: ['Ruby on Rails','CSS','JavaScript'],
-    technologiesDesktop: ['Codekit','Github','JavaScript','Bootstrap','Terminal','Codepen'],
+    technologiesMobile: ['Ruby on Rails', 'CSS', 'JavaScript'],
+    technologiesDesktop: ['Codekit', 'Github', 'JavaScript', 'Bootstrap', 'Terminal', 'Codepen'],
     sourceLink: '#',
     liveLink: '#',
-},
+  },
 
-{
+  {
     projectId: 3,
-    gridClass : "w3",
+    gridClass: 'w3',
     name: 'Keeping tracks of hunders of components',
     projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     imageDesktop: './Images/Snapshoot-Portfolio.svg',
     imageMobile: './Images/mobile-snapshoot.svg',
-    githubIcon:'./Images/gitHubIcon.png',
+    githubIcon: './Images/gitHubIcon.png',
     liveIcon: './Images/liveIcon.svg',
-    technologiesMobile: ['Ruby on Rails','CSS','JavaScript'],
-    technologiesDesktop: ['Codekit','Github','JavaScript','Bootstrap','Terminal','Codepen'],
+    technologiesMobile: ['Ruby on Rails', 'CSS', 'JavaScript'],
+    technologiesDesktop: ['Codekit', 'Github', 'JavaScript', 'Bootstrap', 'Terminal', 'Codepen'],
     sourceLink: '#',
     liveLink: '#',
-},
+  },
 
-{
+  {
     projectId: 4,
-    gridClass : "w4",
+    gridClass: 'w4',
     name: 'Keeping tracks of hunders of components',
     projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     imageDesktop: './Images/Snapshoot-Portfolio.svg',
     imageMobile: './Images/mobile-snapshoot.svg',
-    githubIcon:'./Images/gitHubIcon.png',
+    githubIcon: './Images/gitHubIcon.png',
     liveIcon: './Images/liveIcon.svg',
-    technologiesMobile: ['Ruby on Rails','CSS','JavaScript'],
-    technologiesDesktop: ['Codekit','Github','JavaScript','Bootstrap','Terminal','Codepen'],
+    technologiesMobile: ['Ruby on Rails', 'CSS', 'JavaScript'],
+    technologiesDesktop: ['Codekit', 'Github', 'JavaScript', 'Bootstrap', 'Terminal', 'Codepen'],
     sourceLink: '#',
     liveLink: '#',
-},
+  },
 
-{
+  {
     projectId: 5,
-    gridClass : "w5",
+    gridClass: 'w5',
     name: 'Keeping tracks of hunders of components',
     projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     imageDesktop: './Images/Snapshoot-Portfolio.svg',
     imageMobile: './Images/mobile-snapshoot.svg',
-    githubIcon:'./Images/gitHubIcon.png',
+    githubIcon: './Images/gitHubIcon.png',
     liveIcon: './Images/liveIcon.svg',
-    technologiesMobile: ['Ruby on Rails','CSS','JavaScript'],
-    technologiesDesktop: ['Codekit','Github','JavaScript','Bootstrap','Terminal','Codepen'],
+    technologiesMobile: ['Ruby on Rails', 'CSS', 'JavaScript'],
+    technologiesDesktop: ['Codekit', 'Github', 'JavaScript', 'Bootstrap', 'Terminal', 'Codepen'],
     sourceLink: '#',
     liveLink: '#',
-},
+  },
 
-{
+  {
     projectId: 6,
-    gridClass : "w6",
+    gridClass: 'w6',
     name: 'Keeping tracks of hunders of components',
     projectDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     imageDesktop: './Images/Snapshoot-Portfolio.svg',
     imageMobile: './Images/mobile-snapshoot.svg',
-    githubIcon:'./Images/gitHubIcon.png',
+    githubIcon: './Images/gitHubIcon.png',
     liveIcon: './Images/liveIcon.svg',
-    technologiesMobile: ['Ruby on Rails','CSS','JavaScript'],
-    technologiesDesktop: ['Codekit','Github','JavaScript','Bootstrap','Terminal','Codepen'],
+    technologiesMobile: ['Ruby on Rails', 'CSS', 'JavaScript'],
+    technologiesDesktop: ['Codekit', 'Github', 'JavaScript', 'Bootstrap', 'Terminal', 'Codepen'],
     sourceLink: '#',
     liveLink: '#',
-},
+  },
 ];
-  
-  const worksGrid = document.querySelector('.recent-work');
-  
-  for (let i = 0; i < worksInfo.length; i++) {
-    
-    worksGrid.innerHTML+= `
+
+const worksGrid = document.querySelector('.recent-work');
+
+for (let i = 0; i < worksInfo.length; i += 1) {
+  worksGrid.innerHTML += `
     <div class="work-1-container ${worksInfo[i].gridClass}">
     <img class="project-image" src="./Images/img-work.svg" alt="project picture">
     <div class="work-bottom-cont">
@@ -111,28 +110,49 @@ const worksInfo = [
     </div>
     </div>
     `;
-  }
-  
-  const popup= document.querySelector('.popup');
-  function popupHTML(id)
-  {
-    popup.style.display= 'flex';
-    popup.innerHTML=`
-    <img class="popup-image-dsk" src="${worksInfo[id].imageDesktop}" alt="Project Image">
-    <img class="popup-image-mbl" src="${worksInfo[id].imageMobile}" alt="Project Image">
+}
+
+const popup = document.querySelector('.popup');
+// eslint-disable-next-line no-unused-vars
+function popupHTML(id) {
+  popup.style.display = 'flex';
+  popup.innerHTML = `
+    <div class="popup-background">
+    <div class="close-button-container">
+    <img class="popup-close-button" src="./Images/Disabled.svg" alt="Popup close button">
+    </div>
+    <div class"container-popup">
+    <img class="popup-image-dsk " src="${worksInfo[id].imageDesktop}" alt="Project Image">
+    <img class="popup-image-mbl " src="${worksInfo[id].imageMobile}" alt="Project Image">
+    <div id="title-btn">
     <div id="title-div">
     <h2 class="popup-title">${worksInfo[id].name}</h2>
     </div>
+    <div class="popup-button-desktop">
+    <button class="a1">See live<img class="button-icon" src="${worksInfo[id].liveIcon}"></button>
+    <button class="a1" id="btn2">See Source<img class="button-icon" src="${worksInfo[id].githubIcon}"></button>
+    </div>
+    </div>
+    <div class="tags">
     <ul class="tags-desktop" id="mytech">
         ${worksInfo[id].technologiesDesktop.map((ele) => `<li>${ele}</>`)}
         </ul>
         <ul class="tags-mobile" id="mobile-ul">
         ${worksInfo[id].technologiesMobile.map((ele) => `<li>${ele}</>`)}
-        </ul>
+    </ul>
+    </div>
+    <div class="desc">
     <p class="popup-description">${worksInfo[id].projectDescription}</p>
-    <button>See live<img class="button-icon" src="${worksInfo[id].liveIcon}"></button>
-    <button id="btn2">See Source<img class="button-icon" src="${worksInfo[id].githubIcon}"></button>
-    
+    </div>
+    <div class="popup-button-mobile">
+    <button class="a1">See live<img class="button-icon" src="${worksInfo[id].liveIcon}"></button>
+    <button class="a1" id="btn2">See Source<img class="button-icon" src="${worksInfo[id].githubIcon}"></button>
+    </div>
+    </div>
+    </div>
     `;
-  }
-  
+  const popupClose = document.querySelector('.popup-close-button');
+  popupClose.addEventListener('click', () => {
+    popup.style.display = 'none';
+  });
+}

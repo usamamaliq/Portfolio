f6123087f3cbf028285f79430391cdebc0429856
Usama Masood <usamamaliq@gmail.com>
2022-09-15 17:52:21 +0500

    Add fuction to check email validation

---
+++ form-validation.js
@@ -0,0 +1,12 @@
+const submitBtn = document.querySelector('.f-button')
+
+
+// Function to check if email is valid
+
+const validEmail = (email) => {
+  if (email.match(/^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/)) {
+    return true;
+  }
+  return false;
+};
+
